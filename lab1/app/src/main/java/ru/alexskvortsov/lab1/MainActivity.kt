fa2a4da4e140dd41561ca533c868f3d4349e895b
AlexSkvor <megoskvor95@yandex.ru>
2020-02-06 10:21:47 +0200

    answer

--- lab1/app/src/main/java/ru/alexskvortsov/lab1/MainActivity.kt
+++ lab1/app/src/main/java/ru/alexskvortsov/lab1/MainActivity.kt
@@ -1,11 +1,60 @@
 package ru.alexskvortsov.lab1
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : LoggingLifecycleActivity() {
+
+    private companion object {
+        const val MY_PERMISSIONS_REQUEST_CALL_PHONE = 1
+    }
+
+    private var phone = "+79052469003"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv.setOnClickListener {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), MY_PERMISSIONS_REQUEST_CALL_PHONE)
+            } else callPhone()
+        }
     }
+
+    private fun callPhone(number: String = this.phone) {
+        val intent = Intent(Intent.ACTION_CALL).apply {
+            data = Uri.parse("tel:$number")
+        }
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_CALL_PHONE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    callPhone()
+                } else {
+                    // разрешение от пользователя не получено, необходимо отключить этот функционал
+                }
+                return
+            }
+        }
+    }
+
+
 }
