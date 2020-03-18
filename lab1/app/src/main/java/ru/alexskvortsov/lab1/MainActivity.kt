a656d6e596796ed9aa8a653586456b534b6cb56a
AlexSkvor <megoskvor95@yandex.ru>
2020-03-18 10:47:43 +0200

    so ru question 1096168

--- lab1/app/src/main/java/ru/alexskvortsov/lab1/MainActivity.kt
+++ lab1/app/src/main/java/ru/alexskvortsov/lab1/MainActivity.kt
@@ -1,11 +1,15 @@
 package ru.alexskvortsov.lab1
 
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : LoggingLifecycleActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView.colorFilter = ColorMatrixColorFilter(ColorMatrix().apply { setSaturation(0f)})
     }
 }
